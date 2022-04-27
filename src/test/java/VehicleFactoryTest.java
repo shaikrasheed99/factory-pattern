f423d0cc2aaeb9881ddcfdd4c4376e40fc0f88f8
rasheed <rasheed.shaik@thoughtworks.com>
2022-04-27 19:51:42 +0530

    Refactor introduce before each test method in factory test class

--- src/test/java/VehicleFactoryTest.java
+++ src/test/java/VehicleFactoryTest.java
@@ -1,12 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VehicleFactoryTest {
+    private VehicleFactory vehicleFactory;
+
+    @BeforeEach
+    void setUpVehicleFactory() {
+        vehicleFactory = new VehicleFactory();
+    }
+
     @Test
     void shouldBeAbleToCreateBikeWithTwoWheels() {
-        VehicleFactory vehicleFactory = new VehicleFactory();
-
         Vehicle bike = vehicleFactory.createVehicleWithNumberOfWheels(WHEELS.BIKE_WHEELS);
 
         assertEquals(WHEELS.BIKE_WHEELS, bike.getWheels());
